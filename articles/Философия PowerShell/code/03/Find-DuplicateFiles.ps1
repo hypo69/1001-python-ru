4e9ddbf2569eb2039bc6b49735f7338fef71588a
Новое Имя <@email.com>
2025-08-01 19:33:20 +0300

    checkpoint

---
+++ articles/Философия PowerShell/code/03/Find-DuplicateFiles.ps1
@@ -0,0 +1,16 @@
+function Find-DuplicateFiles {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    
+    Get-ChildItem $Path -File -Recurse -ErrorAction SilentlyContinue | 
+        Group-Object Name, Length | 
+        Where-Object { $_.Count -gt 1 } | 
+        ForEach-Object {
+            # ЭТО ИСПРАВЛЕННАЯ СТРОКА:
+            # Внутри оператора $() переменные не экранируются.
+            Write-Host "Найдены дубликаты: $($_.Name)" -ForegroundColor Yellow
+            $_.Group | Select-Object FullName, Length, LastWriteTime
+        }
+}
