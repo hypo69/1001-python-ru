0f574a30bd191bedb5f69c6dcf9a4af959574c8a
hypo69 <hypo69@davidka.net>
2025-08-19 08:43:40 +0300

    checkpoint

---
+++ 1001-python-games/src/ai/__init__.py
@@ -0,0 +1,15 @@
+## \file /src/ai/__init__.py
+# -*- coding: utf-8 -*-
+
+#! .pyenv/bin/python3
+
+"""
+module: src.ai
+	:platform: Windows, Unix
+	:synopsis: Модуль интерфейса моделей ИИ.
+	"""
+
+
+from .gemini import GoogleGenerativeAI
+from .openai import OpenAIModel
+
---
+++ 1001-python-games/src/endoints/bots/telegram/__init__.py
@@ -0,0 +1,14 @@
+## \file /src/bots/telegram/__init__.py
+# -*- coding: utf-8 -*-
+
+#! .pyenv/bin/python3
+
+"""
+.. module:: src.bots.telegram 
+	:platform: Windows, Unix
+	:synopsis:
+
+"""
+MODE = 'dev'
+
+from .bot import TelegramBot
